50b5632242999318dc1c3a69db03ccf0c91caf0d
pantera <alex5612+gitkraken@hotmail.fr>
2017-10-14 01:26:37 +0200

    change to materialize

--- public/js/loadSW.js
+++ public/js/loadSW.js
@@ -14,18 +14,18 @@
 
 	if('serviceWorker' in navigator)
 	{
-		navigator.serviceWorker.register('/sw')
-		.then(function(reg)
-		{
-			// registration worked
-			console.log('Registration succeeded. Scope is ' + reg.scope);
-
-		})
-		.catch(function(error)
-		{
-			// registration failed
-			console.log('Registration failed with ' + error);
-		});
+		// navigator.serviceWorker.register('/sw')
+		// .then(function(reg)
+		// {
+		// 	// registration worked
+		// 	console.log('Registration succeeded. Scope is ' + reg.scope);
+		//
+		// })
+		// .catch(function(error)
+		// {
+		// 	// registration failed
+		// 	console.log('Registration failed with ' + error);
+		// });
 	}
 
 	window.addEventListener('offline', function()
